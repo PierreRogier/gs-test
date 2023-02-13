d3cd93f32e5cb509077b972d27381acaa06ced95
PierreRogier <pierre.rogier3@gmail.com>
2023-02-13 22:21:54 +0100

    quick add menu for phone screens

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,11 +1,18 @@
 import { useAuth } from "@/contexts";
+import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import styles from "./Header.module.scss";
+
+const SIDEBAR_LINKS = [
+	{ path: "/resources", name: "ressources" },
+	{ path: "/resources/create", name: "création" },
+];
 
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	const { isLoggedIn, logOut, user } = useAuth();
 	const router = useRouter();
 
@@ -17,9 +24,55 @@ const Header: FC<HeaderProps> = () => {
 		<header className={styles.wrapper}>
 			<h3>Greenshield</h3>
 			{isLoggedIn && (
-				<div className={styles.infos}>
-					<p>{user?.email}</p>
-					<button className="btn btn-sm btn-secondary" onClick={handleLogout}>logout</button>
+				<>
+					<div className={styles.infos}>
+						<p>{user?.email}</p>
+						<button className="btn btn-sm btn-secondary" onClick={handleLogout}>
+							logout
+						</button>
+					</div>
+					<div className={styles.phoneMenu}>
+						<button
+							className="btn btn-sm btn-secondary"
+							onClick={() => setIsMenuOpen((prev) => !prev)}
+						>
+							menu
+						</button>
+					</div>
+				</>
+			)}
+			{isLoggedIn && (
+				<div className={`${styles.menu} ${isMenuOpen && styles.menuOpen}`}>
+					<header>
+						<button
+							className="btn btn-sm btn-secondary"
+							onClick={() => setIsMenuOpen((prev) => !prev)}
+						>
+							X
+						</button>
+					</header>
+					<div className={`${styles.menuInfos}`}>
+						<p>{user?.email}</p>
+						<button className="btn btn-sm btn-secondary" onClick={handleLogout}>
+							logout
+						</button>
+					</div>
+					<nav>
+						{SIDEBAR_LINKS.map((l) => {
+							return (
+								<button
+									className={styles.link}
+									key={l.name}
+									onClick={() => {
+										setIsMenuOpen(false);
+										router.push(l.path);
+									}}
+								>
+									{l.name}
+								</button>
+							);
+						})}
+					</nav>
 				</div>
 			)}
 		</header>
